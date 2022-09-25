f2be1bf65c7a863651498e3c73bf3d06c1b04ab3
Hiren Joshi <101968967+HirenJoshi16@users.noreply.github.com>
2022-09-25 19:30:35 +0530

    Add files via upload

---
+++ starPattern_4.cpp
@@ -0,0 +1,22 @@
+// Pattern Problem 3 ~ coded by vHiren
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver code
+int main(){
+
+    // Tracks row
+    for(int i=1; i<=5; i++){
+        // Tracks column
+        for(int j=1; j<=5; j++){
+            if(j>=i){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        } 
+        cout<<endl;
+    }
+    return 0;
+}
